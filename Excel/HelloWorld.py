71a711013742a6343061b852fa83a7cddc48e7d5
nuiben <bgporte@outlook.com>
2022-05-18 14:17:04 -0500

    hw_update

--- Excel/HelloWorld.py
+++ Excel/HelloWorld.py
@@ -1,9 +1,11 @@
 from openpyxl import Workbook
 
+filename = "hello_world.xlsx"
+
 workbook = Workbook()
 sheet = workbook.active
 
 sheet["A1"] = "hello"
 sheet["B1"] = "world!"
 
-workbook.save(filename="hello_world.xlsx")
+workbook.save(filename=filename)
